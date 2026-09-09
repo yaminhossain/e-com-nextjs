912d79776c697a1911d786dfe00c8011b95c8969
yaminhossain <yaminhossain.work@gmail.com>
2026-09-09 20:35:56 +0600

    cont: cn function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Button from "@/components/atoms/Button";
+import { cn } from "@/utils/helper";
 
 export default function Home() {
+  console.log(cn("Ok"))
+
   return (
     <div>
       <Button>Hello</Button>
---
+++ src/utils/helper.ts
@@ -0,0 +1,3 @@
+export function cn(...input){
+  return input
+}
